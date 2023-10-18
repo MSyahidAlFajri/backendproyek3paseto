8bbf2a8b216a4a0194c17f53956424d34313c335
MSyahidAlFajri <masyahida963@gmail.com>
2023-10-18 22:03:32 +0700

    anjass

---
+++ config.go
@@ -0,0 +1,28 @@
+package backendproyek3paseto
+
+import (
+	"os"
+
+	"github.com/aiteung/atdb"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func SetConnection(MONGOCONNSTRINGENV, dbname string) *mongo.Database {
+	var DBmongoinfo = atdb.DBInfo{
+		DBString: os.Getenv(MONGOCONNSTRINGENV),
+		DBName:   dbname,
+	}
+	return atdb.MongoConnect(DBmongoinfo)
+}
+
+func GetAllUser(mongoconn *mongo.Database, collection string) []GeoJson {
+	data := atdb.GetAllDoc[[]GeoJson](mongoconn, collection)
+	return data
+}
+
+func IsPasswordValid(mongoconn *mongo.Database, collection string, userdata User) bool {
+	filter := bson.M{"username": userdata.Username}
+	res := atdb.GetOneDoc[User](mongoconn, collection, filter)
+	return CheckPasswordHash(userdata.Password, res.Password)
+}
